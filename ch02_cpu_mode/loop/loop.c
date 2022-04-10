6c0f1ddfb8d2bf1c82021ecb3ed5bc630ce81686
aki-nishikawa <akihito.n.0910@keio.jp>
2022-04-11 00:06:56 +0900

    ch04

---
+++ ch02_cpu_mode/loop/loop.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("start endless loop\n");
+	for(;;);
+}
---
+++ ch03_process_management/fork-execve/fork-execve.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
+
+static void child()
+{
+	char *args[] = {"/bin/echo", "hello", NULL};
+	printf("I'm child! my pid is %d.\n", getpid());
+	fflush(stdout);
+	execve("/bin/echo", args, NULL);
+	err(EXIT_FAILURE, "failed to exec()");
+}
+
+static void parent(pid_t pid_c)
+{
+	printf("I'm parent! my pid is %d.\n", getpid());
+	printf("\tand, my child's pid is %d.\n", pid_c);
+	exit(EXIT_SUCCESS);
+}
+
+int main(void)
+{
+	pid_t ret;
+	ret = fork();
+
+	if ( ret == -1 ) {
+		err(EXIT_FAILURE, "failed to fork.");
+	}
+
+	if ( ret == 0 ) {
+		/*
+		* child process comes here because
+		* fork() returns 0 for child process
+		*/
+		printf("%d\n", ret);
+		child();
+	} else {
+		/*
+		* parent process comes here because
+		* fork() returns the pid of newly created child process
+		*/
+		printf("%d\n", ret);
+		parent(ret);
+	}
+
+	err(EXIT_FAILURE, "shouldn't reach here");
+}
